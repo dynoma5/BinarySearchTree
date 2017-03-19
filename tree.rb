73f0cd1f3fab17364b1051a02bf7db7878438f0f
David <manakete659@gmail.com>
2017-03-19 19:48:10 -0400

    Finished dfs method

--- tree.rb
+++ tree.rb
@@ -80,19 +80,42 @@ end
 	end
 	
 	def depth_first_search(value, tree)
-		stack = []
+		copy = []
 		check = tree
 		check2 = tree
-		while (stack != [])
-			if check.left_link != nil
+		stack = [tree]
+		while(stack != [])
+			if check.left_link != nil and check.right_link != nil
+				stack.push(check.right_link).push(check.left_link)
+			elsif check.left_link != nil and check.right_link == nil
 				stack.push(check.left_link)
-				check2 = check
-				check = check.left_link
-			elsif check.left_link == nil && check.right_link != nil
-					stack.push(check.right_link)
-					
-				
+			elsif check.left_link == nil and check.right_link != nil
+				stack.push(check.right_link)
+			end
+				check = stack[stack.length-1]
+				copy.push(stack.pop)
+		end	
+		while(copy != [])
+			check2 = copy.shift
+			if check2.value == value
+				return check2
+			end
+		end	
+	end
 	
+	def dfs_rec(value, tree)
+		if tree.value == value
+			return tree 
+		end
+			if tree.left_link != nil
+			 a = dfs_rec(value, tree.left_link)
+			 return a if a != nil
+			 end
+			 if tree.right_link != nil
+			 b = dfs_rec(value, tree.right_link)
+			 return b  if b != nil
+			 end
+	end		
 	
 	
 	
@@ -100,7 +123,11 @@ end
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 tree = build_tree(array)
 node =  breadth_first_search(324, tree)
+node2 = depth_first_search(7, tree)
+node3 = dfs_rec(23,tree)
 print node
+print node2.value
+puts node3.value
 
 
 
